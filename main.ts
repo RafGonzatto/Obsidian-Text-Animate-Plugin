5bd1ee37e5aba805c79e8265db71ce5e28df4754
RafGonzatto <rafaelgonzatto181@gmail.com>
2024-05-22 17:37:24 -0300

    feat: Implement 'Animate Text' plugin for Obsidian
    -This commit implements 'Animate Text' plugin to Obsidian, which enables the simulation of typing text in a file from start to finish.

--- main.ts
+++ main.ts
@@ -1,134 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView, Notice, PluginSettingTab, Setting, App, EditorPosition } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TextAnimateSettings {
+  speed: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TextAnimateSettings = {
+  speed: 50
+};
+
+export default class TextAnimatePlugin extends Plugin {
+  settings: TextAnimateSettings;
+  originalContent: string = '';
+  isActive: number = 1;
+
+  async onload() {
+    console.log('Loading Text Animate Plugin');
+
+    await this.loadSettings();
+
+    this.addCommand({
+      id: 'animate-text',
+      name: 'Animate Text',
+      callback: () => this.animateText(),
+    });
+
+    this.addRibbonIcon('pencil', 'Animate Text', async () => {
+	this.isActive = 1;
+      this.animateText();
+    });
+
+    this.addSettingTab(new TextAnimateSettingTab(this.app, this));
+	
+	window.addEventListener('keydown', (event: KeyboardEvent) => {
+		if (event.keyCode === 27) {
+			this.isActive = 0;
+		}
+	});
+  
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  onunload() {
+    console.log('Unloading Text Animate Plugin');
+  }
 
-	async onload() {
-		await this.loadSettings();
+  async animateText() {
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (activeView) {
+      const editor = activeView.editor;
+      const content = editor.getValue();
+	  this.originalContent = editor.getValue();
+      editor.setValue(''); 
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      let currentIndex = 0;
+      const interval = setInterval(() => {
+        if (currentIndex < content.length && this.isActive === 1) {
+          const pos: EditorPosition = editor.offsetToPos(currentIndex);
+          editor.replaceRange(content[currentIndex], pos);
+          editor.setCursor(pos); 
+          const range = { from: pos, to: pos }; 
+          editor.scrollIntoView(range);
+          currentIndex++;
+        } else {
+          clearInterval(interval);
+		  this.cancelAnimation()
+        }
+      }, this.settings.speed);
+    } else {
+      new Notice('No active markdown view to animate.');
+    }
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  cancelAnimation() {
+	this.isActive = 1;
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (activeView) {
+        const editor = activeView.editor;
+        editor.setValue(this.originalContent); 
+    }
+}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TextAnimateSettingTab extends PluginSettingTab {
+  plugin: TextAnimatePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: TextAnimatePlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    let { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    containerEl.createEl('h2', { text: 'Text Animate Plugin Settings' });
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Animation Speed')
+      .setDesc('Set the speed of the animation (in milliseconds per character).')
+      .addText(text => text
+        .setPlaceholder('50')
+        .setValue(this.plugin.settings.speed.toString())
+        .onChange(async (value) => {
+          this.plugin.settings.speed = parseInt(value);
+          await this.plugin.saveSettings();
+        }));
+  }
 }
